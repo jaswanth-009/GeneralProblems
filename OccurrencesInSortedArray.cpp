3ac5837a3d617b3450002d812136dec3d16ee00f
Jaswanth Kunjeti <kunjetijassvanthh@gmail.com>
2022-05-14 19:21:18 +0530

    Create OccurrencesInSortedArray.cpp

---
+++ OccurrencesInSortedArray.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	vector<int> vec = {1,2,2,3,4,5,5,5,5,6,9,10,14,22,22,24};
+	int key = 5;
+
+	auto low = lower_bound(vec.begin(), vec.end(), key);
+	if(low == vec.end() || *low != key) {
+		cout << "Key not found\n";
+	}
+
+	auto high = upper_bound(vec.begin(), vec.end(), key);
+
+	cout << high - low << "\n";
+}
